a9bd7b0bcc0947894a541ad8901171b0521916dd
Leonid Zhartun <blink_182@inbox.ru>
2023-04-08 00:54:41 +0300

    7

--- src/Components/Auth/AuthForm/index.tsx
+++ src/Components/Auth/AuthForm/index.tsx
@@ -7,29 +7,51 @@ export const AuthForm = () => {
   const [formstate, setformstate]=useState({
     email:'',
     password:''
+    
+    
   })
 
+  const [isError, setIsError]= useState(false);
+
   const handleChangeFieldForm = (fieldName:string)=>(e:React.ChangeEvent<HTMLInputElement>)=>{
+      setformstate((prevValues)=>({...prevValues, [fieldName]:e.target.value }))
+  };
 
+
+  const handleFinishFieldForm = ()=>{
+    if(formstate.password.length<5){
+      setIsError(true)
+    }else{
+    console.log('Auth');
   }
+    
+  };
 
+  useEffect(()=>{
+    if(formstate.password.length>5&& isError ){
+      setIsError(false)
+    }
+  },[formstate.password])
 
   return (
     <div>
       <CustomInput
         fieldLabel="Email" 
-        fieldName='Email'  
-        value='' 
+        fieldName='email'  
+        value={formstate.email}
+        type="email"
+        handleChangeFieldForm={handleChangeFieldForm}
             
       />
       <CustomInput
         fieldLabel="Password"
         fieldName="password"
-        value=''
+        value={formstate.password}
         type="password"
-        
+        handleChangeFieldForm={handleChangeFieldForm}
       />
-      
+      <span className={`${styles.errorTitle} ${isError? styles.hasError:styles.noError}`}>Пароль меньше 5 символов надо</span>
+      <button onClick={handleFinishFieldForm}>Auth</button>
     </div>
   );
 };
